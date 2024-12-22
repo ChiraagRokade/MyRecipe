d41713ef9c274ac274c87726e1ad6c11b0dc26c1
Chiraag Rokade <94799807+ChiraagRokade@users.noreply.github.com>
2024-12-23 01:25:15 +0530

    added view Model Scope

--- app/src/main/java/com/example/myrecipe/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/myrecipe/viewModel/MainViewModel.kt
@@ -3,11 +3,18 @@ package com.example.myrecipe.viewModel
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.myrecipe.model.Category
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
     private val _categoriesState = mutableStateOf(RecipeState())
     val categoriesState: State<RecipeState> = _categoriesState
+
+    private fun fetchCategories(){
+        viewModelScope.launch {
+        }
+    }
 
     data class RecipeState(
         val isLoading: Boolean = false,

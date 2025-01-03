01d7f697e4739f12cf7991c7bb9888a99cc90351
Chiraag Rokade <94799807+ChiraagRokade@users.noreply.github.com>
2025-01-03 13:44:39 +0530

    added parcelize

--- app/src/main/java/com/example/myrecipe/model/Category.kt
+++ app/src/main/java/com/example/myrecipe/model/Category.kt
@@ -1,10 +1,14 @@
 package com.example.myrecipe.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Category(
     val id:String,
     val strCategory:String,
     val strCategoryThumb:String,
     val strCategoryDescription:String
-)
+):Parcelable
 
 data class CategoriesResponse(val categories: List<Category>)
